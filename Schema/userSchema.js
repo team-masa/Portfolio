307efe4d290b93d8ae6ee34637ce1bde74520158
sarah amedior <amediorsarah@gmail.com>
2024-07-16 05:06:36 +0000

    schema

--- Schema/userSchema.js
+++ Schema/userSchema.js
@@ -10,6 +10,6 @@ export const userSchema= joi.object({
     password: joi.string().min(4).required(),
     confirmPassword: joi.ref('password'),
     userName: joi.string().required(),
-    termsAndConditions: joi.boolean().required(),
+    termsAndConditions: joi.boolean(),
     user: joi.string().required()
 });
